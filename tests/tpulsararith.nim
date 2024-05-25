1a3aa86cd008962d7bfcad22b968eb4396f79437
xTrayambak <xtrayambak@disroot.org>
2024-05-25 17:09:22 +0530

    (add) new tests

--- tests/tpulsararith.nim
+++ tests/tpulsararith.nim
@@ -4,8 +4,17 @@ import mirage/runtime/pulsar/interpreter
 
 let i = newPulsarInterpreter("""
 CLAUSE main
-  1 LOADI   1 0  # control integer
-  2 
+  1 LOADI   1 0 
+  2 LOADI   2 3 
+  3 GTI     1 2  
+  4 JUMP    6 
+  5 JUMP    9 
+  6 LOADS   3 "0 > 3" 
+  7 CALL    print 3 
+  8 RETURN  NULL 
+  9 LOADS   3 "0 < 3" 
+  10 CALL   print 3 
+  11 RETURN NULL 
 END main
 """)
 
