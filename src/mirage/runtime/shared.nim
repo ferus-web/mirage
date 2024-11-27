bc345e6cf6c9ca183a5ee07341f1cd6e1edea33e
xTrayambak <xtrayambak@disroot.org>
2024-11-27 21:05:41 +0530

    (add) `SequenceReadIndex` opcode

--- src/mirage/runtime/shared.nim
+++ src/mirage/runtime/shared.nim
@@ -229,6 +229,9 @@ type
     ## Subtract two floats
     SubFloat = 0x33
 
+    ## Read a value off of a sequence atom
+    SequenceReadIndex = 0x34
+
 const
   OpCodeToTable* = {
     "CALL": Call,
@@ -278,7 +281,8 @@ const
     "DIVF": DivFloat,
     "POWF": PowerFloat,
     "ADDF": AddFloat,
-    "SUBF": SubFloat
+    "SUBF": SubFloat,
+    "SRIX": SequenceReadIndex
   }.toTable
 
   OpCodeToString* = static:
