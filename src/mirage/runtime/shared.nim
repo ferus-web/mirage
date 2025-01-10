7f4607c820312f267b2b30391fd44c2f2080a158
xTrayambak <xtrayambak@disroot.org>
2025-01-10 19:02:04 +0530

    (fix) fix compilation error

--- src/mirage/runtime/shared.nim
+++ src/mirage/runtime/shared.nim
@@ -284,8 +284,7 @@ const
     "POWF": PowerFloat,
     "ADDF": AddFloat,
     "SUBF": SubFloat,
-    "ZRETV": ZeroRetval,
-    "LOADBI": LoadBigInt
+    "ZRETV": ZeroRetval
   }.toTable
 
   OpCodeToString* = static:
